9a17f11b78623143f0369ba1bedfab8db2954320
Brandon Weaver <a7xncob@gmail.com>
2020-05-02 02:02:10 -0400

    Prepare view model functionality for data binding.

---
+++ WeatherAppUI/ViewModels/ViewModelParents/NotificationBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppUI.ViewModels.ViewModelParents
+{
+    class NotificationBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WeatherAppUI/ViewModels/WeatherObjectViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAppUI.Models;
+using WeatherAppUI.ViewModels.ViewModelParents;
+
+namespace WeatherAppUI.ViewModels
+{
+    class WeatherObjectViewModel : NotificationBase
+    {
+        private WeatherObject _weatherObject;
+
+        public WeatherObject WeatherObject
+        {
+            get { return this._weatherObject; }
+            set
+            {
+                this._weatherObject = value;
+                this.OnPropertyChanged(nameof(this.WeatherObject));
+            }
+        }
+
+        private string _input;
+
+        public string Input
+        {
+            get { return this._input; }
+            set
+            {
+                this._input = value;
+                this.OnPropertyChanged(nameof(this.Input));
+            }
+        }
+
+        public WeatherObjectViewModel()
+        {
+            this.WeatherObject = new WeatherObject
+            {
+                Data = new List<Data>()
+            };
+        }
+    }
+}
