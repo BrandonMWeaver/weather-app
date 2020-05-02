fb5136a46fef518ffdc5f98bc5f6beb336588ea4
Brandon Weaver <a7xncob@gmail.com>
2020-05-02 01:49:10 -0400

    Add MVVM structure/Add basic weather object for deserialization of API response.

---
+++ WeatherAppUI/Models/WeatherObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppUI.Models
+{
+    class Data
+    {
+        public string City_Name { get; set; }
+        public string State_Code { get; set; }
+    }
+
+    class WeatherObject
+    {
+        public List<Data> Data { get; set; }
+    }
+}
